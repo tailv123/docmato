b8152327f450862417f9c6eb74f0606d45b954c3
donnyboi <diresher@gmail.com>
2020-08-30 15:55:48 +1000

    Don's Commit

--- FAQ-Chatbot-master/main.py
+++ FAQ-Chatbot-master/main.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request
 from dialogue_manager import *
- 
- 
-    
+
+
 app = Flask(__name__)
 dialogue_manager = DialogueManager()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return str(dialogue_manager.generate_answer(userText))
+# Run Program
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=int(80))
+    app.run(host='0.0.0.0', port=int(80))
